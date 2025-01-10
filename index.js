5da2e32dcfc93e327d8d92f70f832b32658433d9
abdurrahim220 <z.abdurrahim5@gmail.com>
2025-01-10 18:13:36 +0600

    cors update

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
-const connectDB = require('./db/connectDB');
-require('dotenv').config()
+const cors = require("cors");
+const connectDB = require("./db/connectDB");
+require("dotenv").config();
 
 const port = 5000;
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json());
-app.use(cors())
+const corsOptions = {
+  origin: ["http://localhost:5000", "https://your-frontend-domain.com", "*"],
+  methods: ["GET", "POST", "PUT", "DELETE",],
+  allowedHeaders: ["Content-Type", "Authorization"], 
+};
 
+app.use(cors(corsOptions));
 
-app.use('/api', require('./routes/categoryRoutes'));
-app.use('/api', require('./routes/productsRoutes'));
-app.use('/api', require('./routes/stripe'));
-app.use('/api', require('./routes/user'));
-app.use('/api', require('./routes/order'));
+app.use("/api", require("./routes/categoryRoutes"));
+app.use("/api", require("./routes/productsRoutes"));
+app.use("/api", require("./routes/stripe"));
+app.use("/api", require("./routes/user"));
+app.use("/api", require("./routes/order"));
 
-app.get("/",(req,res)=>{
-    res.send("Server is running");
-})
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.listen(port,()=>{
-    connectDB()
-    console.log(`server is running on port ${port}`);
-})
+app.listen(port, () => {
+  connectDB();
+  console.log(`server is running on port ${port}`);
+});
